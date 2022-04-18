5aaed742e4c738f34077f84a58ea55929476491c
Сергей Кудашкин <ya.sikuda@yandex.ru>
2022-04-18 23:59:11 +0300

    Initial commit

---
+++ app/src/main/java/ru/sikuda/mobile/foto_wth_coordinates/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.sikuda.mobile.foto_wth_coordinates
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
